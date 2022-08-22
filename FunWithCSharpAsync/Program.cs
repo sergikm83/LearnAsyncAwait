db4ba4cbff517a4425ac056fbdbdec2eb5112c2d
Sergey Misko <spy_3462@mail.ru>
2022-08-22 17:56:17 +0400

    добавил метод DoWorkAsync() и пометил его как async, вызвал метод из Main()

--- FunWithCSharpAsync/Program.cs
+++ FunWithCSharpAsync/Program.cs
@@ -9,6 +9,8 @@ namespace FunWithCSharpAsync
         static async Task Main(string[] args)
         {
             Console.WriteLine("Fun with Async ===>");
+            string message = await DoWorkAsync();
+            Console.WriteLine(message);
             Console.WriteLine(DoWork());
             Console.WriteLine("Completed");
             Console.ReadLine();
@@ -17,6 +19,14 @@ namespace FunWithCSharpAsync
         {
             Thread.Sleep(5000);
             return "Done with work!";
+        }
+        static async Task<string> DoWorkAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                return "Done with work async!";
+            });
         }
     }
 }
